5bb9f7fe8052b64af7479b90017a701427453ca1
Taylor Kingsbury <taylorkingsbury@gmail.com>
2020-04-05 19:38:44 -0400

    adding boilerplate picklist code

--- force-app/main/default/lwc/recordTypeFilter/recordTypeFilter.js
+++ force-app/main/default/lwc/recordTypeFilter/recordTypeFilter.js
@@ -1,3 +1,23 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire} from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import Type_FIELD from '@salesforce/schema/Account.Type';
 
-export default class RecordTypeFilter extends LightningElement {}
+export default class RecordTypeFilter extends LightningElement {
+    @track value;
+
+    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    objectInfo;
+
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: Type_FIELD})
+    TypePicklistValues;
+
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: INDUSTRY_FIELD})
+    IndustryPicklistValues;
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+}
