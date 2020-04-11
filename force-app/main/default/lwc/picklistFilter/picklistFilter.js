9fa4bf77164c3c63ec70fcecb58eaf8cc60acbc7
Taylor Kingsbury <taylorkingsbury@gmail.com>
2020-04-11 16:15:01 -0400

    renaming component

--- force-app/main/default/lwc/picklistFilter/picklistFilter.js
+++ force-app/main/default/lwc/picklistFilter/picklistFilter.js
@@ -3,15 +3,15 @@ import { getPicklistValues } from 'lightning/uiObjectInfoApi';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
-import Type_FIELD from '@salesforce/schema/Account.Type';
+import TYPE_FIELD from '@salesforce/schema/Account.Type';
 
-export default class RecordTypeFilter extends LightningElement {
+export default class PicklistFilter extends LightningElement {
     @track value;
 
     @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
     objectInfo;
 
-    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: Type_FIELD})
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: TYPE_FIELD})
     TypePicklistValues;
 
     @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: INDUSTRY_FIELD})
